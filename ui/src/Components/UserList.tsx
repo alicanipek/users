5cee73c051fecda1978472d4221115986374549e
Ali Can İpek <alicanipek@outlook.com.tr>
2020-03-02 20:47:11 +0300

    adds delete operation support to ui

--- ui/src/Components/UserList.tsx
+++ ui/src/Components/UserList.tsx
@@ -1,55 +1,21 @@
 import React from "react";
 import "../App.css";
-import { Table, Divider } from "antd";
+import { Table, Divider, message, Popconfirm } from "antd";
 
 import { ColumnProps } from "antd/es/table";
 import { Link } from "react-router-dom";
 
 type User = {
-	userId: number;
-	name: string;
-	age: number;
-	city: string;
+	UserID: number;
+	Name: string;
+	Age: number;
+	City: string;
 };
 
 type UserListState = {
 	dataSource: User[];
 };
 
-const tableColumns: ColumnProps<User>[] = [
-	{
-		title: "ID",
-		dataIndex: "UserID",
-		key: "userid"
-	},
-	{
-		title: "Name",
-		dataIndex: "Name",
-		key: "name"
-	},
-	{
-		title: "Age",
-		dataIndex: "Age",
-		key: "age"
-	},
-	{
-		title: "City",
-		dataIndex: "City",
-		key: "city"
-	},
-	{
-		title: "Action",
-		key: "action",
-		render: record => {
-			return (
-				<span>
-					<Link to={`/users/${record.UserID}`}>Edit</Link>
-					<Divider type="vertical" />
-				</span>
-			);
-		}
-	}
-];
 export class UserList extends React.Component<{}, UserListState> {
 	constructor(p: {}) {
 		super(p);
@@ -57,11 +23,78 @@ export class UserList extends React.Component<{}, UserListState> {
 			dataSource: []
 		};
 	}
+	tableColumns: ColumnProps<User>[] = [
+		{
+			title: "ID",
+			dataIndex: "UserID",
+			key: "userid"
+		},
+		{
+			title: "Name",
+			dataIndex: "Name",
+			key: "name"
+		},
+		{
+			title: "Age",
+			dataIndex: "Age",
+			key: "age"
+		},
+		{
+			title: "City",
+			dataIndex: "City",
+			key: "city"
+		},
+		{
+			title: "Action",
+			key: "action",
+			render: record => {
+				return (
+					<span>
+						<button>
+							<Link to={`/users/${record.UserID}`}>Edit</Link>
+						</button>
+						<Divider type="vertical" />
+						<Popconfirm
+							title="Are you sure delete this task?"
+							onConfirm={e => this.handleDelete(record.UserID)}
+							okText="Yes"
+							cancelText="No"
+						>
+							<button>
+								<a href="#">Delete</a>
+								{record.UserID}
+							</button>
+						</Popconfirm>
+					</span>
+				);
+			}
+		}
+	];
+	handleDelete(UserID: number) {
+		fetch("http://localhost:8000/user/" + UserID, {
+			method: "DELETE", // 'POST' or 'PUT'
+			headers: {
+				"Content-Type": "application/json"
+			}
+		})
+			.then(() => {
+				message.success("Successfully deleted " + UserID + ".");
+				const dataSource = [...this.state.dataSource];
+				this.setState({
+					dataSource: dataSource.filter(
+						item => item.UserID !== UserID
+					)
+				});
+			})
+			.catch(error => {
+				message.error("Error occured: " + error);
+			});
+	}
 
 	render() {
 		return (
 			<Table
-				columns={tableColumns}
+				columns={this.tableColumns}
 				dataSource={this.state.dataSource}
 				rowKey="UserID"
 			/>
